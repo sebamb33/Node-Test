58f52d6d48ae6ac26ea16bfd90f16adc483f78f4
sebastien ambona <sebastien.ambona33@gmail.com>
2022-04-24 13:53:15 +0200

    Insertion des pokemons

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("coucou");
+const express = require('express');
+let pokemons = require('./mock-pokemon');
+const app = express()
+const port = 3000
+app.get('/',(req,res) =>res.send("Coucou tout le monde !"));
+app.get('/api/pokemons/:id',(req,res)=>{
+   const id =parseInt(req.params.id);
+   const pokemon = pokemons.find (pokemon=>pokemon.id===id)
+    res.send('Vous avez demandé le pokémon '+pokemon.name)
+});
+app.listen(port,()=>console.log("Notre application node est lancée ! "))
