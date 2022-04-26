fc9b79aa5a42e99e1ead3e0a619a6f21955fe590
sebastien ambona <sebastien.ambona33@gmail.com>
2022-04-26 07:28:05 +0200

    Ajout des pokemon via put

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const {success} = require('./helper');
+const {success,getUniqueId} = require('./helper');
 const morgan = require('morgan');
+const favicon=require("serve-favicon");
+const bodyParser = require('body-parser');
 let pokemons = require('./mock-pokemon');
 const app = express()
 const port = 3000
 
 //Création d'un middleware
-app.use(morgan('dev'));
+app
+    .use(favicon(__dirname+'/favicon.ico'))
+    .use(morgan('dev'))
+    .use(bodyParser.json())
 app.get('/',(req,res) =>res.send("Coucou tout le monde !"));
 app.get('/api/pokemons/:id',(req,res)=>{
    const id =parseInt(req.params.id);
@@ -18,4 +23,25 @@ app.get('/api/pokemons/',(req,res)=>{
     let message='Tout les pokémons on étaient chargé';
     res.json(success(message,pokemons))
 })
+//Ajouter un pokemon via l'api rest
+app.post('/api/pokemons',(req,res)=>
+{
+    const id =parseInt(getUniqueId(pokemons))
+    const pokemonCreated  = {...req.body,...{id: id, created: new Date()}};
+    pokemons.push(pokemonCreated)
+    const message = 'Le pokémon à '+pokemonCreated+'bien était crée';
+    res.json(success(message,pokemonCreated));
+})
+//modification d'un pokemon
+app.put('/api/pokemons/:id',(req,res)=>
+{
+    const id =parseInt(req.params.id);
+    const pokemonUpdated  ={...req.body,id:id}
+    pokemons = pokemons.map(pokemon=>{
+        return pokemon.id===id ? pokemonUpdated:pokemon
+    })
+    const message = 'Le pokemon '+pokemonUpdated+'à bien était modifié.';
+    res.json(success(message,pokemonUpdated));
+})
+app.delete(/api/pokemons)
 app.listen(port,()=>console.log("Notre application node est lancée ! "))
--- helper.js
+++ helper.js
@@ -1,1 +1,12 @@
 exports.success = (message,data) => {return {message, data}}
+
+//Ajout ici car méthode outil
+
+
+exports.getUniqueId=(pokemons)=>{
+    const pokemonsIds= pokemons.map(pokemon=>pokemon.id)
+    //Récupérer la   valeur la plus haute
+    const maxId= pokemonsIds.reduce((a,b)=>Math.max(a,b))
+    const uniqueId = maxId+1
+    return uniqueId
+}
