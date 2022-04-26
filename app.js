e886269f087d7190ddb9466f1def4efe84e71576
sebastienAmb <sebastien.ambona33@gmail.com>
2022-04-26 13:57:47 +0200

    Suppression d'un pokemon et connexion à la base de données

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ const express = require('express');
 const {success,getUniqueId} = require('./helper');
 const morgan = require('morgan');
 const favicon=require("serve-favicon");
+const {Sequelize} = require('sequelize');
 const bodyParser = require('body-parser');
 let pokemons = require('./mock-pokemon');
 const app = express()
 const port = 3000
+//Initialisation de sequelize
+const sequelize = new Sequelize('pokedex','root','root',
+    {
+        host:'localhost',
+        dialect:'mysql',
+        dialectOptions:
+            {
+                timezone:'Etc/GMT-2',
+                socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+            },
+        logging:false
+
+    })
+
+sequelize.authenticate().then(_=>"La connexion est bonne")
+    .catch(error=>console.error('Connexion impossible :'+error))
 
 //Création d'un middleware
 app
@@ -43,5 +60,12 @@ app.put('/api/pokemons/:id',(req,res)=>
     const message = 'Le pokemon '+pokemonUpdated+'à bien était modifié.';
     res.json(success(message,pokemonUpdated));
 })
-app.delete(/api/pokemons)
+app.delete("/api/pokemons/:id",(req,res)=>
+{
+    const id = parseInt(req.params.id)
+    const pokemonDelete=pokemons.find(pokemon=>pokemon.id===id)
+    pokemons.filter(pokemon =>pokemon.id!==id)
+    const message = 'Le pokémon '+pokemonDelete.name+' à était supprimé';
+    res.json(success(message,pokemonDelete))
+})
 app.listen(port,()=>console.log("Notre application node est lancée ! "))
